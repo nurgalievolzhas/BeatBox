58d9db2cefa1977f3f51304b5c4fa656c710dbac
nurgalievol <nurgalievol33@gmail.com>
2021-05-24 17:05:38 +0600

    Initial commit

---
+++ app/src/test/java/com/hfad/beatbox/SoundViewModelTest.java
@@ -0,0 +1,40 @@
+package com.hfad.beatbox;
+
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class SoundViewModelTest {
+
+    //Instances
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+
+    @Test
+    public void exposesSoundNameAsTitle(){
+        assertThat(mSubject.getTitle(), is(mSound.getName()));
+    }
+
+    @Test
+    public void callsBeatBoxPlayOnButtonClicked(){
+        mSubject.onButtonClicked();
+
+        verify(mBeatBox).play(mSound);
+    }
+
+}
